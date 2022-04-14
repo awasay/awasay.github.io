94eeec506a86ea60edc7acc655d58244425ef0ff
awasay <abdul.wasay@intel.com>
2022-04-13 17:51:37 -0700

    moved

--- tutorial-mpr/src/index.js
+++ tutorial-mpr/src/index.js
@@ -1,6 +1,14 @@
 new gridjs.Grid({
     columns: ["MP Systems", "Data Source(s)", "Learning Type", "MP Pillars", "Stage", "Application Domain"],
+    sort:true,
+    search:true,
     data: [
+      ["Copilot", 
+      "Code + documentation", 
+      "Self-supervised",
+      "Intention + Invention",
+      "Development",
+      "General"],
       ["Copilot", 
       "Code + documentation", 
       "Self-supervised",
